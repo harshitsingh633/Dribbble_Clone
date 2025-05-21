8761e904493ecc5f282320e276cc324e0b4a656b
Harshit singh <hs8700882@gmail.com>
2025-05-21 23:18:06 +0530

    Initial Code

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <nav>
+        <h1>Dribbble</h1>
+        <p>Explore <i className="arrow down"></i> </p>
+        <p>Hire a Designer <i className="arrow down"></i></p>
+        <p>Find Jobs</p>
+        <p>Blog</p>
+        <button id='Signup'>Sign up</button>
+        <button id='login'>Log in</button>
+        </nav>
+
+
+
+      <div className='hero'>
+        <span>Discover the world’s <br />
+        <span> top designers</span>
+        </span>
+        <p>Explore work from the most talented and accomplished designers <br />ready to take on your next project</p>
+      </div>
+
+      <div className='Search-box'>
+        <input type="search" name="Search" id="" />
+      </div>
+
+    </div>
+  )
+}
+
+export default App
